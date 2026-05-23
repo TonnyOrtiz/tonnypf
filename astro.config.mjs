90b10544a733e9c217239adcd84b9bbc9189fa3d
tos <tonny.ortiz.95@gmail.com>
2026-05-23 05:03:00 -0600

    Added third job experience and switch to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,8 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });
